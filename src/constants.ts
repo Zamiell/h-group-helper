5aed39584cfe2f973670b816f5a54e741f109811
Zamiell <5511220+Zamiell@users.noreply.github.com>
2022-01-31 15:43:41 -0500

    cleanup

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,5 @@
 const cwd = process.cwd();
 
 export const CWD = cwd;
-export const MAX_NUM_VOICE_CHANNELS = 50;
 export const PROJECT_NAME = "H-Group Helper";
 export const VOICE_CHANNEL_PREFIX = "H-Group ";
