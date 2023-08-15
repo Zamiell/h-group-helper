5755bed425d5ac9ea03b31496a2e6b5a94775f2e
James <5511220+Zamiell@users.noreply.github.com>
2023-08-15 10:59:27 -0400

    chore: update scripts

--- restart.sh
+++ restart.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-set -e # Exit on any errors
+set -euo pipefail # Exit on errors and undefined variables.
 
-# Get the directory of this script
+# Get the directory of this script:
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# Get the name of the repository
+# Get the name of the repository:
 # https://stackoverflow.com/questions/23162299/how-to-get-the-last-part-of-dirname-in-bash/23162553
-REPO="$(basename "$DIR")"
+REPO_NAME="$(basename "$DIR")"
 
-SECONDS=0
-
-cd "$DIR"
-"$DIR/build.sh"
-pm2 restart $REPO
+bash "$DIR/build.sh"
+pm2 restart "$REPO_NAME"
