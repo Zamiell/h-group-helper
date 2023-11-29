e7967ec01eb76d065f2dded95e91803dd7c25879
James <5511220+Zamiell@users.noreply.github.com>
2023-11-29 11:39:22 -0500

    refactor: simplify env

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,2 @@
-import path from "node:path";
-import url from "node:url";
-
-const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
-
-export const REPO_ROOT = path.join(__dirname, "..");
 export const PROJECT_NAME = "H-Group Helper";
 export const VOICE_CHANNEL_PREFIX = "H-Group ";
