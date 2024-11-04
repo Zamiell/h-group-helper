7a35db21eb600c6d2a63de45af30d55e8c807c3a
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-11-04 09:43:24 -0500

    feat: add link for role-explanations

--- src/discordUtils.ts
+++ src/discordUtils.ts
@@ -110,6 +110,6 @@ export async function sendDMWithDeletedMessage(
 export async function sendNotHGroupDM(message: Message): Promise<void> {
   const dmChannel = await message.author.createDM();
   const dmMessage =
-    'Your post in the convention-proposals forum has been deleted because you do not have the "H-Group" role. Do you regularly play pick-up games in this Discord server using the voice channels? If so, please send a direct message to a moderator to request the "H-Group" role. You can find the current list of moderators in the #role-explanations channel.';
+    'Your post in the convention-proposals forum has been deleted because you do not have the "H-Group" role. Do you regularly play pick-up games in this Discord server using the voice channels? If so, please send a direct message to a moderator to request the "H-Group" role. You can find the current list of moderators in the [#role-explanations](<https://discord.com/channels/140016142600241152/930525271780638791/930696130579283978>) channel.';
   await sendDMWithDeletedMessage(dmChannel, dmMessage, message.content);
 }
