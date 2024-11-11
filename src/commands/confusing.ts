d617bf1b1eab165d9ea5324147c1e64ac9528bbd
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-11-10 22:30:12 -0500

    update

--- src/commands/confusing.ts
+++ src/commands/confusing.ts
@@ -3,7 +3,7 @@ import { SlashCommandBuilder } from "discord.js";
 import type { Command } from "../interfaces/Command.js";
 
 const CONFUSING_MESSAGE =
-  'A surprising number of questions in the #convention-questions channel boil down to some form of: "Is my complicated clue technically legal?" These kind of questions can be interesting, but the person asking the question is often [missing the forest for the trees](<https://www.merriam-webster.com/dictionary/miss%20the%20forest%20for%20the%20trees>): just because a clue is "technically legal" does **not** mean that you should give it. The **most important section of the doc** is the section on [_Clarity Principle_](<https://hanabi.github.io/level-6/#clarity-principle>). This principle is more important than all other principles.';
+  'A surprising number of convention questions boil down to some form of: "Is my complicated clue technically legal?" These kind of questions can be interesting, but the person asking the question is often [missing the forest for the trees](<https://www.merriam-webster.com/dictionary/miss%20the%20forest%20for%20the%20trees>): just because a clue is "technically legal" does **not** mean that you should give it. The **most important section of the doc** is the section on [_Clarity Principle_](<https://hanabi.github.io/level-6/#clarity-principle>).';
 
 export const confusingCommand: Command = {
   data: new SlashCommandBuilder()
